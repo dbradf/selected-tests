74be634d7d57b2e6577dab108ae23ad17260dd22
Lydia Stepanek <lydia.stepanek@mongodb.com>
2019-10-08 19:08:36 -0400

    remove more doc strings

--- src/selectedtests/test_mappings/find_revisions.py
+++ src/selectedtests/test_mappings/find_revisions.py
@@ -11,24 +11,12 @@ LOGGER = get_logger(__name__)
 def _add_project_revision_for_version(
     version: Version, project_revisions_to_analyze: list
 ):
-    """
-    Build a dictionary of tasks that flipped for builds in this version.
-
-    :param work_item: Container of work items to analyze.
-    :return: RevisionPair of what tasks flipped.
-    """
     project_revisions_to_analyze.append(version.revision)
 
 
 def _add_module_revision_for_version(
     version: Version, module_repo: str, module_revisions_to_analyze: list
 ):
-    """
-    Build a dictionary of tasks that flipped for builds in this version.
-
-    :param work_item: Container of work items to analyze.
-    :return: RevisionPair of what tasks flipped.
-    """
     modules = version.get_manifest().modules
     for module in modules:
         if modules[module].repo == module_repo:
@@ -43,16 +31,6 @@ def _add_module_revision_for_version(
 def add_revisions_for_project(
     evg_api: EvergreenApi, project: str, start_date: int, module_repo: str = ""
 ) -> Dict:
-    """
-    Find test flips in the evergreen project.
-
-    :param project: Evergreen project to analyze.
-    :param look_back: Look at commits until the given project.
-    :param evg_api: Evergreen API.
-    :param n_threads: Number of threads to use.
-    :return: Dictionary of commits that introduced task flips.
-    """
-    LOGGER.debug("Starting find_flips iteration")
     version_iterator = evg_api.versions_by_project(project)
     project_revisions_to_analyze = []
     module_revisions_to_analyze = []
