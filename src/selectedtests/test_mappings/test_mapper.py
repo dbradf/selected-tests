a96b6f35080d5935d8db6346eb2930b454390075
Lydia Stepanek <lydia.stepanek@mongodb.com>
2019-10-09 11:16:39 -0400

    investigate issue with intemittently repeating enterprise git shas

--- src/selectedtests/test_mappings/test_mapper.py
+++ src/selectedtests/test_mappings/test_mapper.py
@@ -29,8 +29,8 @@ class TestMapper(object):
 
     @classmethod
     def create_mappings(cls, repo, revisions, test_re, source_re, start_date, project, branch):
-        file_intersection = defaultdict(lambda: defaultdict(int))
-        file_count = defaultdict(int)
+        file_intersection = defaultdict(lambda: defaultdict(list))
+        file_count = defaultdict(list)
 
         LOGGER.debug("searching until", ts=start_date)
         commit_count = 0
@@ -59,9 +59,9 @@ class TestMapper(object):
                     src_changed.add(path)
 
             for src in src_changed:
-                file_count[src] += 1
+                file_count[src].append(commit.id)
                 for test in tests_changed:
-                    file_intersection[src][test] += 1
+                    file_intersection[src][test].append(commit.id)
 
         return TestMapper(file_intersection, file_count, commit_count, project, repo, branch)
 
