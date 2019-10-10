2550f9f1eff386bb413b237de26d45c4ca2379d8
Lydia Stepanek <lydia.stepanek@mongodb.com>
2019-10-10 17:29:28 -0400

    make test file smaller

--- tests/selectedtests/test_mappings/test_test_mapper.py
+++ tests/selectedtests/test_mappings/test_test_mapper.py
@@ -13,6 +13,9 @@ import selectedtests.test_mappings.test_mapper as under_test
 CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 SOURCE_RE = re.compile(".*source")
 TEST_RE = re.compile(".*test")
+START_DATE = datetime.combine(datetime.now() - timedelta(days=1), time())
+PROJECT = "my_PROJECT"
+BRANCH = "master"
 
 
 def initialize_temp_repo(directory):
@@ -96,38 +99,29 @@ def repo_with_one_source_file_and_one_test_file_changed_in_different_commits():
 class TestTestMapper:
     def test_create_mappings_with_no_source_files_changed(self, repo_with_no_source_files_changed):
         revisions = [commit.hexsha for commit in repo_with_no_source_files_changed.iter_commits('master')]
-        start_date = datetime.combine(datetime.now() - timedelta(days=1), time())
-        project = "my_project"
-        branch = "master"
-        test_mappings = under_test.TestMapper.create_mappings(repo_with_no_source_files_changed, revisions, TEST_RE, SOURCE_RE, start_date, project, branch)
+        test_mappings = under_test.TestMapper.create_mappings(repo_with_no_source_files_changed, revisions, TEST_RE, SOURCE_RE, START_DATE, PROJECT, BRANCH)
         test_mappings_list = test_mappings.get_mappings()
         assert len(test_mappings_list) == 0
 
 
     def test_create_mappings_with_one_source_file_and_no_test_files_changed(self, repo_with_one_source_file_and_no_test_files_changed):
         revisions = [commit.hexsha for commit in repo_with_one_source_file_and_no_test_files_changed.iter_commits('master')]
-        start_date = datetime.combine(datetime.now() - timedelta(days=1), time())
-        project = "my_project"
-        branch = "master"
-        test_mappings = under_test.TestMapper.create_mappings(repo_with_one_source_file_and_no_test_files_changed, revisions, TEST_RE, SOURCE_RE, start_date, project, branch)
-        test_mappings_list = test_mappings.get_mappings() #  assert len(test_mappings_list) == 0 #  def test_create_mappings_with_no_source_files_and_one_test_file_changed(self, repo_with_no_source_files_and_one_test_file_changed): #  revisions = [commit.hexsha for commit in repo_with_no_source_files_and_one_test_file_changed.iter_commits('master')] #  start_date = datetime.combine(datetime.now() - timedelta(days=1), time()) #  project = "my_project" #  branch = "master" #  test_mappings = under_test.TestMapper.create_mappings(repo_with_no_source_files_and_one_test_file_changed, revisions, TEST_RE, SOURCE_RE, start_date, project, branch) #  test_mappings_list = test_mappings.get_mappings()
+        test_mappings = under_test.TestMapper.create_mappings(repo_with_one_source_file_and_no_test_files_changed, revisions, TEST_RE, SOURCE_RE, START_DATE, PROJECT, BRANCH)
+        test_mappings_list = test_mappings.get_mappings() #  assert len(test_mappings_list) == 0 #  def test_create_mappings_with_no_source_files_and_one_test_file_changed(self, repo_with_no_source_files_and_one_test_file_changed): #  revisions = [commit.hexsha for commit in repo_with_no_source_files_and_one_test_file_changed.iter_commits('master')] #  START_DATE = datetime.combine(datetime.now() - timedelta(days=1), time()) #  PROJECT = "my_PROJECT" #  BRANCH = "master" #  test_mappings = under_test.TestMapper.create_mappings(repo_with_no_source_files_and_one_test_file_changed, revisions, TEST_RE, SOURCE_RE, START_DATE, PROJECT, BRANCH) #  test_mappings_list = test_mappings.get_mappings()
         assert len(test_mappings_list) == 0
 
 
     def test_create_mappings_with_one_source_file_and_one_test_file_changed_in_same_commit(self, repo_with_one_source_file_and_one_test_file_changed_in_same_commit):
         git_repo = repo_with_one_source_file_and_one_test_file_changed_in_same_commit
         revisions = [commit.hexsha for commit in git_repo.iter_commits('master')]
-        start_date = datetime.combine(datetime.now() - timedelta(days=1), time())
-        project = "my_project"
-        branch = "master"
-        test_mappings = under_test.TestMapper.create_mappings(git_repo, revisions, TEST_RE, SOURCE_RE, start_date, project, branch)
+        test_mappings = under_test.TestMapper.create_mappings(git_repo, revisions, TEST_RE, SOURCE_RE, START_DATE, PROJECT, BRANCH)
         test_mappings_list = test_mappings.get_mappings()
 
         source_file_test_mapping = test_mappings_list[0]
         assert source_file_test_mapping['source_file'] == 'new-source-file'
-        assert source_file_test_mapping['project'] == project
+        assert source_file_test_mapping['project'] == PROJECT
         assert source_file_test_mapping['repo'] == os.path.basename(git_repo.working_dir)
-        assert source_file_test_mapping['branch'] == branch
+        assert source_file_test_mapping['branch'] == BRANCH
         assert source_file_test_mapping['source_file_seen_count'] == 1
         for test_file_mapping in source_file_test_mapping['test_files']:
             assert test_file_mapping['name'] == 'new-test-file'
@@ -137,9 +131,6 @@ class TestTestMapper:
     def test_create_mappings_with_one_source_file_and_one_test_file_changed_in_different_commits(self, repo_with_one_source_file_and_one_test_file_changed_in_different_commits):
         repo = repo_with_one_source_file_and_one_test_file_changed_in_different_commits
         revisions = [commit.hexsha for commit in repo.iter_commits('master')]
-        start_date = datetime.combine(datetime.now() - timedelta(days=1), time())
-        project = "my_project"
-        branch = "master"
-        test_mappings = under_test.TestMapper.create_mappings(repo, revisions, TEST_RE, SOURCE_RE, start_date, project, branch)
+        test_mappings = under_test.TestMapper.create_mappings(repo, revisions, TEST_RE, SOURCE_RE, START_DATE, PROJECT, BRANCH)
         test_mappings_list = test_mappings.get_mappings()
         assert len(test_mappings_list) == 0
