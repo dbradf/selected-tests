445516bbdf810ef56852c892900db40c430421e2
Lydia Stepanek <lydia.stepanek@mongodb.com>
2019-10-10 14:16:56 -0400

    tests are definitely run concurrently because this fails

--- tests/selectedtests/test_mappings/test_test_mapper.py
+++ tests/selectedtests/test_mappings/test_test_mapper.py
@@ -11,7 +11,6 @@ from datetime import datetime, time, timedelta
 import selectedtests.test_mappings.test_mapper as under_test
 
 CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-TEMP_REPO_DIRECTORY = os.path.join(CURRENT_DIRECTORY, 'my-temp-repo')
 SOURCE_RE = re.compile(".*source")
 TEST_RE = re.compile(".*test")
 
@@ -29,7 +28,7 @@ def destroy_temp_repo(directory):
 
 @pytest.fixture(scope="module")
 def repo_with_no_source_files_changed():
-    temp_directory = os.path.join(CURRENT_DIRECTORY,"no_source_files_changed")
+    temp_directory = os.path.join(CURRENT_DIRECTORY, "no_source_files_changed")
     repo = initialize_temp_repo(temp_directory)
     yield repo
 
@@ -38,12 +37,25 @@ def repo_with_no_source_files_changed():
 
 @pytest.fixture(scope="module")
 def repo_with_one_source_file_and_no_test_files_changed():
-    temp_directory = os.path.join(CURRENT_DIRECTORY,"one_source_file_and_no_test_files_changed")
+    temp_directory = os.path.join(CURRENT_DIRECTORY, "one_source_file_and_no_test_files_changed")
     repo = initialize_temp_repo(temp_directory)
     source_file = os.path.join(temp_directory, 'new-source-file')
     open(source_file, 'wb').close()
     repo.index.add([source_file])
-    repo.index.commit("add source")
+    repo.index.commit("add source file")
+    yield repo
+
+    destroy_temp_repo(temp_directory)
+
+
+@pytest.fixture(scope="module")
+def repo_with_no_source_files_and_one_test_file_changed():
+    temp_directory = os.path.join(CURRENT_DIRECTORY, "one_source_file_and_no_test_files_changed")
+    repo = initialize_temp_repo(temp_directory)
+    test_file = os.path.join(temp_directory, 'new-test-file')
+    open(test_file, 'wb').close()
+    repo.index.add([test_file])
+    repo.index.commit("add test file")
     yield repo
 
     destroy_temp_repo(temp_directory)
@@ -51,7 +63,7 @@ def repo_with_one_source_file_and_no_test_files_changed():
 
 @pytest.fixture(scope="module")
 def repo_with_one_source_file_and_one_test_file_changed_in_same_commit():
-    temp_directory = os.path.join(CURRENT_DIRECTORY,"one_source_file_and_one_test_file_changed_in_same_commit")
+    temp_directory = os.path.join(CURRENT_DIRECTORY, "one_source_file_and_one_test_file_changed_in_same_commit")
     repo = initialize_temp_repo(temp_directory)
     source_file = os.path.join(temp_directory, 'new-source-file')
     test_file = os.path.join(temp_directory, 'new-test-file')
@@ -85,7 +97,16 @@ class TestTestMapper:
         assert len(test_mappings_list) == 0
 
 
-    #  def test_create_mappings_with_no_source_files_and_one_test_file_changed(self, git_repo):
+    def test_create_mappings_with_no_source_files_and_one_test_file_changed(self, repo_with_no_source_files_and_one_test_file_changed):
+        revisions = [commit.hexsha for commit in repo_with_no_source_files_and_one_test_file_changed.iter_commits('master')]
+        start_date = datetime.combine(datetime.now() - timedelta(days=1), time())
+        project = "my_project"
+        branch = "master"
+        test_mappings = under_test.TestMapper.create_mappings(repo_with_no_source_files_and_one_test_file_changed, revisions, TEST_RE, SOURCE_RE, start_date, project, branch)
+        test_mappings_list = test_mappings.get_mappings()
+        assert len(test_mappings_list) == 0
+
+
     #  def test_create_mappings_with_one_source_file_and_one_test_file_changed_in_different_commits
     def test_create_mappings_with_one_source_file_and_one_test_file_changed_in_same_commit(self, repo_with_one_source_file_and_one_test_file_changed_in_same_commit):
         git_repo = repo_with_one_source_file_and_one_test_file_changed_in_same_commit
