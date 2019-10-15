bb6e92548c3b7537426bce0415fc79f367b866cd
Lydia Stepanek <lydia.stepanek@mongodb.com>
2019-10-15 16:17:15 -0400

    test timerange

--- tests/selectedtests/test_mappings/test_test_mapper.py
+++ tests/selectedtests/test_mappings/test_test_mapper.py
@@ -12,8 +12,8 @@ import selectedtests.test_mappings.test_mapper as under_test
 CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 SOURCE_RE = re.compile(".*source")
 TEST_RE = re.compile(".*test")
-START_DATE = datetime.combine(datetime.now() - timedelta(days=3), time())
-END_DATE = datetime.combine(datetime.now() + timedelta(days=1), time())
+ONE_DAY_AGO = datetime.combine(datetime.now() - timedelta(days=1), time())
+ONE_DAY_FROM_NOW = datetime.combine(datetime.now() + timedelta(days=1), time())
 PROJECT = "my_project"
 BRANCH = "master"
 
@@ -99,14 +99,35 @@ def repo_with_one_source_file_and_one_test_file_changed_in_different_commits():
     destroy_temp_repo(temp_directory)
 
 
+@pytest.fixture(scope="module")
+def repo_with_files_added_two_days_ago():
+    two_days_ago = str(datetime.combine(datetime.now() - timedelta(days=2), time()))
+    os.environ["GIT_AUTHOR_DATE"] = two_days_ago
+    os.environ["GIT_COMMITTER_DATE"] = two_days_ago
+
+    temp_directory = os.path.join(
+        CURRENT_DIRECTORY, "one_source_file_and_one_test_file_changed_in_same_commit_two_days_ago"
+    )
+    repo = initialize_temp_repo(temp_directory)
+    source_file = os.path.join(temp_directory, "new-source-file")
+    test_file = os.path.join(temp_directory, "new-test-file")
+    open(source_file, "wb").close()
+    open(test_file, "wb").close()
+    repo.index.add([source_file, test_file])
+    repo.index.commit("add source and test file in same commit")
+    yield repo
+
+    destroy_temp_repo(temp_directory)
+
+
 class TestTestMapper:
     def test_no_source_files_changed(self, repo_with_no_source_files_changed):
         test_mappings = under_test.TestMapper.create_mappings(
             repo_with_no_source_files_changed,
             TEST_RE,
             SOURCE_RE,
-            START_DATE,
-            END_DATE,
+            ONE_DAY_AGO,
+            ONE_DAY_FROM_NOW,
             PROJECT,
             BRANCH,
         )
@@ -120,8 +141,8 @@ class TestTestMapper:
             repo_with_one_source_file_and_no_test_files_changed,
             TEST_RE,
             SOURCE_RE,
-            START_DATE,
-            END_DATE,
+            ONE_DAY_AGO,
+            ONE_DAY_FROM_NOW,
             PROJECT,
             BRANCH,
         )
@@ -135,8 +156,8 @@ class TestTestMapper:
             repo_with_no_source_files_and_one_test_file_changed,
             TEST_RE,
             SOURCE_RE,
-            START_DATE,
-            END_DATE,
+            ONE_DAY_AGO,
+            ONE_DAY_FROM_NOW,
             PROJECT,
             BRANCH,
         )
@@ -150,8 +171,8 @@ class TestTestMapper:
             repo_with_one_source_file_and_one_test_file_changed_in_same_commit,
             TEST_RE,
             SOURCE_RE,
-            START_DATE,
-            END_DATE,
+            ONE_DAY_AGO,
+            ONE_DAY_FROM_NOW,
             PROJECT,
             BRANCH,
         )
@@ -176,8 +197,45 @@ class TestTestMapper:
             repo_with_one_source_file_and_one_test_file_changed_in_different_commits,
             TEST_RE,
             SOURCE_RE,
-            START_DATE,
-            END_DATE,
+            ONE_DAY_AGO,
+            ONE_DAY_FROM_NOW,
+            PROJECT,
+            BRANCH,
+        )
+        test_mappings_list = test_mappings.get_mappings()
+        assert len(test_mappings_list) == 0
+
+    def test_date_range_includes_time_of_file_changes(
+        self, repo_with_files_added_two_days_ago
+    ):
+        three_days_ago = datetime.combine(datetime.now() - timedelta(days=3), time())
+        two_days_ago = datetime.combine(datetime.now() - timedelta(days=2), time())
+        test_mappings = under_test.TestMapper.create_mappings(
+            repo_with_files_added_two_days_ago,
+            TEST_RE,
+            SOURCE_RE,
+            three_days_ago,
+            two_days_ago,
+            PROJECT,
+            BRANCH,
+        )
+        test_mappings_list = test_mappings.get_mappings()
+
+        source_file_test_mapping = test_mappings_list[0]
+        assert source_file_test_mapping["source_file"] == "new-source-file"
+        for test_file_mapping in source_file_test_mapping["test_files"]:
+            assert test_file_mapping["name"] == "new-test-file"
+            assert test_file_mapping["test_file_seen_count"] == 1
+
+    def test_date_range_excludes_time_of_file_changes(
+        self, repo_with_files_added_two_days_ago
+    ):
+        test_mappings = under_test.TestMapper.create_mappings(
+            repo_with_files_added_two_days_ago,
+            TEST_RE,
+            SOURCE_RE,
+            ONE_DAY_AGO,
+            ONE_DAY_FROM_NOW,
             PROJECT,
             BRANCH,
         )
